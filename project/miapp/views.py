ca307f674d943e665ffc7fd7195ee909cecc63ee
Arturo Ferraro <azbendrihel@gmail.com>
2023-07-14 13:55:40 +0200

    agregamos datos.html

--- project/miapp/views.py
+++ project/miapp/views.py
@@ -6,8 +6,24 @@ def index(request):
 
 def formulario(request):
     if request.method == 'POST':
-       
-        return redirect('index') 
+        return redirect('datos')
 
     return render(request, 'miapp/formulario.html')
 
+def datos(request):
+    if request.method == 'POST':
+        pais_de_origen = request.POST['pais_de_origen']
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        dni = request.POST['dni']
+        
+        datos = {
+            'pais_de_origen': pais_de_origen,
+            'nombre': nombre,
+            'apellido': apellido,
+            'dni': dni
+        }
+
+        return render(request, 'miapp/datos.html', datos)
+
+    return redirect('index')
